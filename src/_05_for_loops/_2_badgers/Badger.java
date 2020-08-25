99e8ff2588b0d5b3edb1a4a03f001411398a03f6
bbmma <bbmma@192.168.0.24>
2020-08-24 17:57:37 -0700

    .

--- src/_05_for_loops/_2_badgers/Badger.java
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -1,21 +1,18 @@
 package _05_for_loops._2_badgers;
 
 public class Badger {
-public static void main(String[] args) {
-	for (int i=0; i<11; i++) {
-		System.out.println("badger");
+	public static void main(String[] args) {
+		for (int j = 0; j < 2; j++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print("badger ");
+			}
+System.out.println();
+			for (int i = 0; i < 2; i++) {
+				System.out.print("mushroom ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("A snake!");
 	}
-	
-	for (int i=0; i<2; i++) {
-		System.out.println("mushroom");
-	}
-	for (int i=0; i<11; i++) {
-		System.out.println("badger");
-	}
-	
-	for (int i=0; i<2; i++) {
-		System.out.println("mushroom");
-	}
-	System.out.println("A snake!");
-}
 }
